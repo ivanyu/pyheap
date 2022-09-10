ee0ff4c723dfb1cee789dbae6a12f80a3cc8f28f
Ivan Yurchenko <ivan@ivanyu.me>
2022-09-10 19:45:15 +0300

    Support thread dumping
    Now the heap dump contains threads with stack traces and local variables. Local variables are included and there referents are included in the heap.

--- pyheap/dumper.py
+++ pyheap/dumper.py
@@ -18,8 +18,10 @@ import gc
 import gzip
 import json
 import sys
+import threading
 import time
-from typing import List, Any, NamedTuple
+import traceback
+from typing import List, Any, NamedTuple, Dict, Tuple
 
 """
 This module is executed in the context of the inferior.
@@ -44,11 +46,21 @@ def dump_heap(heap_file: str) -> str:
 def _dump_heap0(heap_file: str) -> str:
     start = time.monotonic()
     visited = 0
-    all_objects = _all_objects()
+
+    gc_tracked_objects = _get_gc_tracked_objects()
+
+    messages = []
+    threads, locals_ = _get_threads_and_locals(messages)
+
+    all_objects = _all_objects(gc_tracked_objects, locals_)
 
     open_func = gzip.open if heap_file.endswith(".gz") else open
     with open_func(heap_file, "wb") as f:
         f.write("{\n".encode("utf-8"))
+
+        f.write('  "threads": '.encode("utf-8"))
+        f.write(json.dumps(threads, indent=2).encode("utf-8"))
+        f.write(",\n".encode("utf-8"))
 
         types = {}
 
@@ -85,10 +97,13 @@ def _dump_heap0(heap_file: str) -> str:
 
         f.write("\n}".encode("utf-8"))
 
-    return f"Heap dumped to {heap_file}. Visited {visited} objects. Took {(time.monotonic() - start):.3f} seconds."
+    result = f"Heap dumped to {heap_file}. Visited {visited} objects. Took {(time.monotonic() - start):.3f} seconds."
+    if messages:
+        result += "\n" + "\n".join(messages) + "\n"
+    return result
 
 
-def _all_objects() -> List[_PyObject]:
+def _get_gc_tracked_objects() -> List[Any]:
     invisible_objects = set()
     invisible_objects.add(id(invisible_objects))
     invisible_objects.add(id(sys.modules["dumper"]))
@@ -105,37 +120,68 @@ def _all_objects() -> List[_PyObject]:
     invisible_objects.add(id(_dump_heap0))
     invisible_objects.add(id(_all_objects))
 
+    return [o for o in gc.get_objects() if id(o) not in invisible_objects]
+
+
+def _get_threads_and_locals(
+    messages: List[str],
+) -> Tuple[List[Dict[str, List[dict]]], List[Any]]:
+    current_frames = sys._current_frames()
+    all_locals: List[Any] = []
+    stack_traces: List[Dict[str, List[dict]]] = []
+    for thread in threading.enumerate():
+        thread_dict = {
+            "thread_name": thread.name,
+            "alive": thread.is_alive(),
+            "daemon": thread.daemon,
+            "stack_trace": None,
+        }
+        stack_traces.append(thread_dict)
+
+        current_thread_frame = current_frames.get(thread.ident)
+        if current_thread_frame is None:
+            messages.append(f"WARNING - stack for thread {thread.name} not found")
+            continue
+
+        thread_dict["stack_trace"] = []
+        for frame, lineno in traceback.walk_stack(current_thread_frame):
+            # Skip the dumper frames, which may be on top of normal frames.
+            if frame.f_code.co_filename == __file__:
+                continue
+
+            all_locals.extend(frame.f_locals.values())
+            thread_dict["stack_trace"].append(
+                {
+                    "file": frame.f_code.co_filename,
+                    "lineno": lineno,
+                    "name": frame.f_code.co_name,
+                    "locals": {
+                        loc_name: id(loc_value)
+                        for loc_name, loc_value in frame.f_locals.items()
+                    },
+                }
+            )
+    return stack_traces, all_locals
+
+
+def _all_objects(gc_tracked_objects: List[Any], locals_: List[Any]) -> List[_PyObject]:
     seen_ids = set()
-    invisible_objects.add(id(seen_ids))
-
     to_visit = []
-    invisible_objects.add(id(to_visit))
-
-    gc_objects = gc.get_objects()
-    invisible_objects.add(id(gc_objects))
-
-    to_visit.extend(gc_objects)
-
+    to_visit.extend(gc_tracked_objects)
+    to_visit.extend(locals_)
     result = []
-    invisible_objects.add(id(result))
 
     while len(to_visit) > 0:
         obj = to_visit.pop()
         obj_id = id(obj)
-        invisible_objects.add(id(obj_id))
 
-        if obj_id in invisible_objects or obj_id in seen_ids:
+        if obj_id in seen_ids:
             continue
 
         seen_ids.add(obj_id)
-
         # Self-references here are fine.
         referents = gc.get_referents(obj)
-        invisible_objects.add(id(referents))
-
         obj = _PyObject(obj=obj, referents=referents)
-        invisible_objects.add(id(obj))
-
         result.append(obj)
         to_visit.extend(referents)
 
