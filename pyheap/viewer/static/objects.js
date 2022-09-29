26db131234d8896f2398a7b41143d8640349b896
Ivan Yurchenko <ivan@ivanyu.me>
2022-09-29 08:07:05 +0300

    [Viewer] Show inbound references
    Closes #30

--- pyheap/viewer/static/objects.js
+++ pyheap/viewer/static/objects.js
@@ -43,119 +43,148 @@ async function getObject(addr) {
     return (await getObjects([addr]))[0];
 }
 
-async function fetchRootReferents() {
-    const rootObject = await getObject(rootAddr);
-    const referents = await getObjects(rootObject.referents);
-    return referents;
-}
-const rootReferents = fetchRootReferents();
+class ObjectTreeView {
+    #topLevelObjects;
+    constructor(root) {
+        this.#topLevelObjects = this.fetchTopLevelObjects(root);
+    }
 
-function createExpandButton(onExpand, onCollapse) {
-    const iconCollapsed = $(`
-        <svg xmlns="http://www.w3.org/2000/svg" width="0.9em" height="0.9em" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
-            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-        </svg>
-    `);
-    const iconExpanded = $(`
-        <svg xmlns="http://www.w3.org/2000/svg" width="0.9em" height="0.9em" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
-            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-        </svg>
-    `);
-
-    const button = $(`<span style="cursor: pointer; "><span>`).append(iconCollapsed);
-    button.collapsed = true;
-    button.disabled = false;
-
-    button.click(async function() {
-        if (button.disabled) {
-            return;
+    async render() {
+        const topLevelList = this.createTreeList(false);
+        for (const obj of (await this.#topLevelObjects)) {
+            const treeNode = this.createTreeNode(obj, null);
+            topLevelList.append(treeNode);
         }
+        return topLevelList;
+    }
 
-        button.disabled = true;
-        if (button.collapsed) {
-            const empty = await onExpand();
-            if (empty) {
-                button.css("visibility", "hidden");
+    createExpandButton(li, onExpand) {
+        const iconCollapsed = $(`
+            <svg xmlns="http://www.w3.org/2000/svg" width="0.9em" height="0.9em" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+            </svg>
+        `);
+        const iconExpanded = $(`
+            <svg xmlns="http://www.w3.org/2000/svg" width="0.9em" height="0.9em" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+            </svg>
+        `);
+
+        const button = $(`<span style="cursor: pointer; "><span>`).append(iconCollapsed);
+        button.collapsed = true;
+        button.disabled = false;
+
+        const tree = this;
+        button.click(async function() {
+            if (button.disabled) {
+                return;
+            }
+
+            button.disabled = true;
+            if (button.collapsed) {
+                const empty = await tree.onExpand(li);
+                if (empty) {
+                    button.css("visibility", "hidden");
+                } else {
+                    button.empty();
+                    button.append(iconExpanded);
+                }
             } else {
                 button.empty();
-                button.append(iconExpanded);
+                button.append(iconCollapsed);
+                await tree.onCollapse(li);
             }
+            button.collapsed = !button.collapsed;
+            button.disabled = false;
+        });
+
+        return button;
+    }
+
+    createTreeList(pad) {
+        const style = pad ? "padding-left: 1em;" : "";
+        return $(`<ul id="referents-list" style="${style}" class="list-unstyled"></ul>`);
+    }
+
+    createTreeNode(object, treeParent) {
+        const li = $(`<li></li>`);
+        if (treeParent === null) {
+            li.treeLevel = 0;
         } else {
-            button.empty();
-            button.append(iconCollapsed);
-            await onCollapse();
+            li.treeLevel = treeParent.treeLevel + 1;
         }
-        button.collapsed = !button.collapsed;
-        button.disabled = false;
-    });
+        li.object = object;
+        const expandButton = this.createExpandButton(li);
+        li.append(expandButton).append(`
+            <a href="/objects/${escapeHtml(object.address)}" title="Address: ${escapeHtml(object.address)}">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+                    <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+                </svg></a>
+            <code>${escapeHtml(object.type)}</code>
+            <span class="inline-object-str text-muted">${escapeHtml(object.str)}</span>
+        `);
+        return li;
+    }
 
-    return button;
-}
+    async onExpand(li) {
+        if (li.treeChildrenList == undefined) {
+            const spinner = $(`
+                <li style="padding-left: 1em;"><div class="spinner-border spinner-border-sm" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div></li>`);
+            li.after(spinner);
 
-function createTreeList(pad) {
-    const style = pad ? "padding-left: 1em;" : "";
-    return $(`<ul id="referents-list" style="${style}" class="list-unstyled"></ul>`);
-}
+            li.treeChildrenList = this.createTreeList(true);
+            const objects = await this.getChildObjects(li);
+            for (const obj of objects) {
+                const treeChild = this.createTreeNode(obj, li);
+                li.treeChildrenList.append(treeChild);
+            }
 
-async function onExpand() {
-    const li = this;
-    if (li.treeChildrenList == undefined) {
-        const spinner = $(`
-            <li style="padding-left: 1em;"><div class="spinner-border spinner-border-sm" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div></li>`);
-        li.after(spinner);
-
-        li.treeChildrenList = createTreeList(true);
-        const objects = await getObjects(li.object.referents);
-        for (const obj of objects) {
-            const treeChild = createTreeNode(obj, li);
-            li.treeChildrenList.append(treeChild);
+            spinner.remove();
         }
 
-        spinner.remove();
+        const empty = li.treeChildrenList.children().length === 0;
+        if (!empty) {
+            li.after(li.treeChildrenList);
+        }
+        return empty;
     }
 
-    const empty = li.treeChildrenList.children().length === 0;
-    if (!empty) {
-        li.after(li.treeChildrenList);
+    async onCollapse(li) {
+        li.treeChildrenList.detach();
     }
-    return empty;
 }
 
-async function onCollapse() {
-    const li = this;
-    li.treeChildrenList.detach();
+class ReferentsObjectTreeView extends ObjectTreeView {
+    async fetchTopLevelObjects(root) {
+        return await getObjects(root.referents);
+    }
+
+    async getChildObjects(li) {
+        return await getObjects(li.object.referents);
+    }
 }
 
-function createTreeNode(object, treeParent) {
-    const li = $(`<li></li>`);
-    if (treeParent === null) {
-        li.treeLevel = 0;
-    } else {
-        li.treeLevel = treeParent.treeLevel + 1;
+class InboundReferencesObjectTreeView extends ObjectTreeView {
+    async fetchTopLevelObjects(root) {
+        return await getObjects(root.inbound_references);
     }
-    li.object = object;
-    const expandButton = createExpandButton(onExpand.bind(li), onCollapse.bind(li));
-    li.append(expandButton).append(`
-        <a href="/objects/${escapeHtml(object.address)}" title="Address: ${escapeHtml(object.address)}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
-                <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
-            </svg></a>
-        <code>${escapeHtml(object.type)}</code>
-        <span class="inline-object-str text-muted">${escapeHtml(object.str)}</span>
-    `);
-    return li;
+
+    async getChildObjects(li) {
+        return await getObjects(li.object.inbound_references);
+    }
 }
 
 $( document ).ready(async function() {
-    $("#referent-tree-spinner").remove();
-    const list = createTreeList(false);
-    $("#referent-tree-panel").append(list);
+    const rootObject = await getObject(rootAddr);
+    const rotv = new ReferentsObjectTreeView(rootObject);
+    const irotv = new InboundReferencesObjectTreeView(rootObject);
 
-    for (const obj of (await rootReferents)) {
-        const treeNode = createTreeNode(obj, null);
-        list.append(treeNode);
-    }
+    $("#referent-tree-panel").append(await rotv.render());
+    $("#referent-tree-spinner").remove();
+
+    $("#inbound-references-tree-panel").append(await irotv.render());
+    $("#inbound-references-tree-spinner").remove();
 });
